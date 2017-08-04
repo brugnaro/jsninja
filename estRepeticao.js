c34c8a7601f1d0204e11d375578546ba5755f651
Marcos Rodrigues Brugnaro <marcos.rodrigues@grupoitc.com.br>
2017-08-03 22:57:46 -0300

    estrutura de repeticao

---
+++ estRepeticao.js
@@ -0,0 +1,12 @@
+var counter=10;
+
+while(counter--){console.log(counter);}
+
+/*while(counter>0){
+    console.log(counter--);
+}*/
+
+/*while(counter<10){
+    console.log(counter);
+    counter++;
+}*/
