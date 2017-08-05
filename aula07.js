f870b385c8ecfc7e8b461543e41b5a3c2f6762df
Marcos Rodrigues Brugnaro <marcos.rodrigues@grupoitc.com.br>
2017-08-05 10:59:27 -0300

    aula07

---
+++ aula07.js
@@ -0,0 +1,29 @@
+/*
+//modulos
+var n=0;
+while(n<=20){
+    n % 2 === 0 ? console.log(n) : '';
+    n++;
+}
+for(var n=0;n<=20;n++){
+    n % 2 === 0 ? console.log(n) : '';
+    n++;
+}
+
+//array.length
+var arr = ['marcos',null,true,{bola:'azul'},()=>{}];
+var qtd=arr.length;
+for(;qtd>0;){
+    console.log(arr[--qtd]);
+    qtd === 3 ? console.log(arr[qtd].bola) : '';
+}
+
+//push
+var arr=[1,2,3,'marcos',{bola:'azul'}];
+arr.push('test',{carro:'BMW'},['1e2','1e3'],soma=(x,y)=>x+y)
+console.log(arr[8](1,2))
+
+for(var a=2,b=3;a<5;a++){
+    console.log(a);
+    console.log(b++);
+}*/
