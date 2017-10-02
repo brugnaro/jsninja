424bee9fce59bcd341f997d3f74075c056e843fe
marcos <marcos@marcosrodriguesbrugnaro@gmail.com>
2017-10-01 21:49:55 -0300

    challenge14 finish

--- aula14.js
+++ aula14.js
@@ -1,15 +1,25 @@
 //reduce()
 //reduceRight()
-var arr = [1,2,3,4,5];
-//var arr = ['m','a','r','c','o','s'];
+/*var arr = ['m','a','r','c','o','s'];
 var reduce = arr.reduceRight(function(acumulado, atual, index, array){
-    return acumulado + atual ;
-});
+    return acumulado + atual;
+});*/
+
 /*
-1ª 0 + 1 = 1
-2ª 1 + 2 = 3
-3ª 3 + 3 = 6
-4ª 6 + 4 = 10
-5ª 10 + 5 = 15
+1ª - 0 + 1 = 1
+2ª - 1 + 2 = 3
+3ª - 3 + 3 = 6
+4ª - 6 + 4 = 10
+5ª - 10 + 5 = 15
 */
-console.log(reduce);
+//console.log(reduce)
+
+//index of procura se um valor existe no indice do array
+//var arr = [1, 2, 3, 4, 5];
+/*var arr = ['b', 'r', 'u', 'g', 'n', 'a', 'r', 'o'];
+var index = arr.lastIndexOf('r');
+console.log(arr[index]);*/
+
+//Array.isArray()
+var arr = ['m','a','r','c','o','s',]
+console.log(typeof true);
