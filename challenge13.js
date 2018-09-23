c0ba63d88cafcf8f404dcc554331f9ed2f10e184
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-09-23 13:34:54 -0300

    Review challenge13

--- challenge13.js
+++ challenge13.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   [1, 2, 3, 4, 5, 6].toString();
 
   var sul = ["parana", "santa catarina", "rio grande do sul"];
@@ -30,12 +30,43 @@
   brasil = brasil.concat(nordeste);
 
   var newBrasil = [];
-  brasil.forEach(function(item, index) {
+  brasil.forEach(function (item, index) {
     newBrasil.push({
-        id: index,
-        estado: item
+      id: index,
+      estado: item
     });
   });
 
-  console.log(newBrasil);
+  console.log(brasil);
+
+  var every = brasil.every(function (item) {
+    return item.length > 4;
+  });
+
+  console.log(
+    every
+      ? 'Sim, todos os estados tem mais de 7 letras!'
+      : 'Nem todos estados tem 7 letras');
+
+  var some = brasil.some(function (item) {
+    return item === 'ceará';
+  });
+
+  console.log(some ? 'Ceará esta incluido!' : 'Ceará não foi incluido');
+
+  var map = newBrasil.map(function (item, index) {
+    return {
+      id: item.id + 1,
+      estado: item.estado + ' pertence ao Brasil'
+    }
+  });
+
+  console.log(map);
+
+  var filter = map.filter(function (item, index) {
+    return item.id % 2 === 0
+  });
+
+  console.log(filter);
+
 })();
